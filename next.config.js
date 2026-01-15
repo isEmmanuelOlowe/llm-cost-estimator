d3a7f015f415043a4069c69984e08b8e7a43cfb2
Emmanuel A. Olowe <e.a.olowe@sms.ed.ac.uk>
2026-01-15 01:03:32 +0000

    github pages

--- next.config.js
+++ next.config.js
@@ -1,44 +1,40 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const baseName = process.env.NEXT_PUBLIC_BASE_PATH || '';
-const basePath = isProd ? baseName : '';
-const assetPrefix = isProd && baseName ? `${baseName}/` : undefined;
+const nextConfig = () => {
+  const isProd = process.env.NODE_ENV === 'production';
+  const baseName = process.env.NEXT_PUBLIC_BASE_PATH || '';
+  const basePath = isProd ? baseName : '';
+  const assetPrefix = isProd && baseName ? `${baseName}/` : undefined;
 
-const nextConfig = {
-  eslint: {
-    dirs: ['src'],
-  },
-
-  reactStrictMode: true,
-  swcMinify: true,
-
-  output: 'export',
-  basePath,
-  assetPrefix,
-  trailingSlash: true,
-
-  images: {
-    unoptimized: true,
-  },
-
-  // SVGR
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            typescript: true,
-            icon: true,
+  return {
+    eslint: {
+      dirs: ['src'],
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'export',
+    basePath,
+    assetPrefix,
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              icon: true,
+            },
           },
-        },
-      ],
-    });
-
-    return config;
-  },
+        ],
+      });
+      return config;
+    },
+  };
 };
 
-module.exports = nextConfig;
+module.exports = nextConfig();
